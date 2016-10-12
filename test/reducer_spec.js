86cee3acd6cf412fcf6d8173d6a753927e5c1bab
Temitope Faro <topriddy@gmail.com>
2016-10-12 04:53:24 +0100

    updated reducer_spec

--- test/reducer_spec.js
+++ test/reducer_spec.js
@@ -57,7 +57,21 @@ describe('reducer', () => {
       },
       entries: []
     }));
+  });
 
+  it('can be used with reduce', () => {
+    const actions = [
+      {type: 'SET_ENTRIES', entries: ['Trainspotting', '28 Days Later']},
+      {type: 'NEXT'},
+      {type: 'VOTE', entry: 'Trainspotting'},
+      {type: 'VOTE', entry: '28 Days Later'},
+      {type: 'VOTE', entry: 'Trainspotting'},
+      {type: 'NEXT'}
+    ];
+    const finalState = actions.reduce(reducer, Map());
+    expect(finalState).to.equal(fromJS({
+      winner: 'Trainspotting'
+    }));
   });
 
 });
